20527e91c93d027320614ddc772669714bc423ff
Merugul-Rahul <122848686+Merugul-Rahul@users.noreply.github.com>
2023-01-23 19:36:02 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,13 +16,54 @@ float f4(float x, int intensity);
 }
 #endif
 
+float charToFloat(char* str){
+  char *remaining;
+  float number = strtof(str, &remaining);
+  if (str == remaining){
+    std::cerr<<"Non numeric character in input!"<<std::endl;
+    exit(0);
+  } 
+  return number;
+}
   
+float calculateIntegral(float functionId, float a, float b, float n, float intensity){
+  float firstPartOfEquation = (b-a)/n;
+  float functionResponse = 0;
+  for (int i=0;i<n;i++){
+    float secondPartOfEquation = a + ((i + 0.5) * firstPartOfEquation);
+    if((int) functionId==1){
+      functionResponse += f1(secondPartOfEquation,intensity);
+    }
+    elseif((int) functionId==2){
+      functionResponse += f2(secondPartOfEquation,intensity);
+    }
+    elseif((int) functionId==3){
+      functionResponse += f3(secondPartOfEquation,intensity);
+    }
+    elseif((int) functionId==3){
+      functionResponse += f4(secondPartOfEquation,intensity);
+    }
+    else{
+      std::cerr<<"Function ID is incorrect."<<std::endl;
+    }
+  }
+  return (firstPartOfEquation*functionResponse);
+}
 int main (int argc, char* argv[]) {
-
   if (argc < 6) {
     std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity>"<<std::endl;
     return -1;
   }
-  
+  float functionId = charToFloat(argv[1]);;
+  float a = charToFloat(argv[2]);
+  float b = charToFloat(argv[3]);
+  int n = charToFloat(argv[4]);
+  int intensity = charToFloat(argv[5]);
+  std::chrono::system_clock::time_point startTime = std::chrono::system_clock::now();
+  float response = (float) calculateIntegral(functionId, a, b, n, intensity);
+  std::chrono::system_clock::time_point endtime = std::chrono::system_clock::now();
+  std::chrono::duration<double, std::milli> difference = endtime - startTime;
+  std::cout<< response<<std::endl;
+  std::cerr << difference.count() << std::endl;
   return 0;
 }
